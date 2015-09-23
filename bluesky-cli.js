4d6aa5fd77dea6992e586148563ec2f81e39c598
Praween AMONTAMAVUT <praween@hykwlab.org>
2015-09-23 13:09:58 +0900

    Try to add sequece and util but not absolutely resolve.

--- bluesky-cli.js
+++ bluesky-cli.js
@@ -8,9 +8,13 @@
  * NOTE: Need fix again. (Problem is the callback feature of 'request' 
  *       and 'http' library cause the random the request messages for 
  *       login-logout to the system (out of control the sequece of 
- *       the operation).)
+ *       the operation).) <-- Try to use async with util in order to 
+ *       arrange the sequence of function execution but it was not
+ *       absolutely resoved the problem. (Need more check it again.)
+ *       (Update: 2015-09-23)
  */
 
+//'use strict';
 /**
  * Declaration of nodejs library
  */
@@ -18,6 +22,11 @@ var querystring = require('querystring');
 var http = require('http');
 var url = require("url");
 var request = require('request');
+var async = require('async');
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+var unirest = require('unirest');
+var sleep = require('sleep');
 
 /**
  * Declaration of class variable.
@@ -33,10 +42,25 @@ var logoutOption;
 
 var retChunk, v;
 
+var _e = {
+	data : 'data',
+	end : 'end',
+	error: 'error',
+	body: 'body',
+	chunk: 'chunk',
+	list_ed: 'list_ed',
+	login_chunk: 'login_chunk',
+	logout_chunk: 'logout_chunk',
+	login_result: 'login_result',
+	logout_result: 'logout_result',
+	sensornetwork_result: 'sensornetwork_result'
+};
+
 /**
  * blueskyconn Constructor.
  */
 function blueskyconn(blueskyGateway, username, password){
+	EventEmitter.call(this);
 	var gatewayUrlParse = url.parse(blueskyGateway);
 
 	if(typeof gatewayUrlParse.port !== 'undefined' && gatewayUrlParse.port !== null){
@@ -84,11 +108,13 @@ function blueskyconn(blueskyGateway, username, password){
 		}
 	};
 }
+util.inherits(blueskyconn, EventEmitter);
 
 /**
  * Blueskyconn Class testing method.
  */
 blueskyconn.prototype.test = function(){
+	var self = this;
 	console.log("test . . .");
 	/*console.log(this.blueskyGateway);
 	console.log(this.blueskyPort);
@@ -99,40 +125,86 @@ blueskyconn.prototype.test = function(){
 	console.log(this.logoutOption);
 	console.log(this.logoutParams);*/
 
-	console.log(this.createBlueskyParam('ls', ["noneFix", "edconnected"]));
+	//console.log(this.createBlueskyParam('ls', ["noneFix", "edconnected"]));
 
-	var gpiotest_opts = ["172.16.4.105", "gpio", "set", "21", "1"];
+	/*var gpiotest_opts = ["172.16.4.105", "gpio", "set", "21", "1"];
 	this.sensornetwork(gpiotest_opts);
+	self.on('sensornetwork_result', function(result){
+		console.log('call sensornetwork API: ' + result);
+	});*/
 
-	//gpiotest_opts = ["172.16.4.105", "gpio", "set", "21", "0"];
-	//this.sensornetwork(gpiotest_opts);
 }
 
-function get(url, callback) {
+//function get(url, callback) {
+blueskyconn.prototype.get = function(url, callback){
 	request({
 		url: url
 	}, function (error, res, body) {
 		if (!error && res.statusCode === 200) {
 			callback(body);
 		}
+		if (error){
+			console.log(error);
+		}
 	});
 }
 
 blueskyconn.prototype.sensornetwork = function(opts){
+	var self = this;
 	var params = this.createBlueskyParam("sensornetwork", opts);
-	this.login();
 
-	get('http://' + this.blueskyGateway + ':' + this.blueskyPort + params, function (body) {
-		console.log(body);
+	async.series([
+	function(callback){
+		self.login();
+		self.on('login_result', function(chunk){
+			//console.log(chunk);
+			
+		});
+		callback(null, "login");
+	},function(callback){
+		var uri = 'http://' + self.blueskyGateway + ':' + self.blueskyPort + params;
+		var reqs = unirest.get(uri).header('Accept', 'text/html')
+			.end(function (response) {
+				var body = response.body;
+				//console.log("GET: " + body);
+				self.emit(_e.sensornetwork_result, response.body);
+				
+			});
+		/*self.get('http://' + self.blueskyGateway + ':' + self.blueskyPort + params, function (body) {
+			console.log(body);
+		});*/
+
+		/*var req = http.get('http://' + self.blueskyGateway + ':' + self.blueskyPort + params, function(res) {
+			// output response body
+			res.setEncoding('utf8');
+			res.on('data', function(str) {
+				console.log(str);
+			});
+		});
+		// error handler
+		req.on('error', function(err) {
+			console.log("Error: " + err.message);
+		});*/
+		callback(null, "Calling API");
+	},function(callback){
+		self.logout();
+		self.on('logout_result', function(chunk){
+			//console.log(chunk);
+		});
+		callback(null, "logout");
+	}], function(err, results){
+		if(err){
+			throw err;
+		}
+		//console.log('series all done. ' + results);
 	});
-
-	this.logout();
 }
 
 /**
  * Return the list of connecting embedded devices information.
  */
 blueskyconn.prototype.list_ed = function(){
+	var self = this;
 	var params = this.createBlueskyParam("ls", ["noneFix", "edconnected"]);
 
 	var getOption = {
@@ -149,20 +221,27 @@ blueskyconn.prototype.list_ed = function(){
 		res.setEncoding('utf8');
 		var body = '';
 	        var jsonBody = '';
-		u = res.on('data', t = function (chunk) {
+		res.on('data', function (chunk) {
 			body += chunk;
 			return;
 		});
 
-		h = res.on('end', function(res){
-			ret = JSON.parse(body);
-		        return console.log(ret);
+		res.on('end', function(res){
+			var ret = JSON.parse(body);
+		        self.emit(_e.body, body);
+			self.emit(_e.list_ed, body);
 		});
-	        return h;
-	});
 
+		res.on('error', function(err){
+			self.emit(_e.error, err);
+		});
+		
+	});
+	
 	listED.write(this.logoutParams);
 	var end = listED.end();
+
+	return self.on('list_ed', function(list_ed){});
 }
 
 /**
@@ -183,8 +262,10 @@ blueskyconn.prototype.createBlueskyParam = function(instruction, opts){
  * Do something with Bluesky API (Underconstruction)
  */
 blueskyconn.prototype.blueskyGet = function(blueskyParam){
-	this.login();
-	this.login();
+	var self = this;
+	
+//	this.login();
+//	this.login();
 
 	var getOption = {
 		host: this.blueskyGateway,
@@ -197,12 +278,10 @@ blueskyconn.prototype.blueskyGet = function(blueskyParam){
 	};
 	var req = http.request(getOption, function(res, callback) {
 		res.setEncoding('utf8');
-		var u, t;
 		var body = '';
-		u = res.on('data', t = function (chunk, callback) {
+		res.on('data', function (chunk, callback) {
 			//console.log(chunk);
 			body += chunk;
-			return;
 		});
 
 		/*res.on('end', function(res){
@@ -212,13 +291,13 @@ blueskyconn.prototype.blueskyGet = function(blueskyParam){
 		return res;
 	});
 
-	req.write(this.logoutParams);
+	req.write("{}");
 	req.end();
 
 	//console.log("blueskyGet: " + req.res);
 
-	this.logout();
-	this.logout();
+//	this.logout();
+//	this.logout();
 
 	return req;
 }
@@ -227,36 +306,112 @@ blueskyconn.prototype.blueskyGet = function(blueskyParam){
  * To login the API.
  */
 blueskyconn.prototype.login = function(){
-	var req = http.request(this.loginOption, function(res) {
-		var t,u;
+	var self = this;
+
+	async.series([
+	function(callback){
+
+		var req = http.request(self.loginOption, function(res) {
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+				//var chunk = chunk;
+				//console.log(chunk);
+				self.emit(_e.login_chunk, chunk);
+				
+			});
+	
+		});
+
+		req.write(self.loginParams);
+		req.end();
+
+		callback();
+	},function(callback){
+
+		var req = http.request(self.loginOption, function(res) {
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+				self.emit(_e.login_chunk, chunk);
+				self.emit(_e.login_result, chunk);
+			});
+	
+		});
+
+		req.write(self.loginParams);
+		req.end();
+
+		callback();
+	}], function(err, results){
+		if(err){
+			throw err;
+		}
+		//console.log('series all done. ' + results);
+	});
+
+	/*var req = http.request(this.loginOption, function(res) {
 		res.setEncoding('utf8');
-		u = res.on('data', t = function (chunk) {
-			//var chunk = chunk;
-			//console.log(chunk);
-			return;
+		res.on('data', function (chunk) {
+			self.emit(_e.login_chunk, chunk);
 		});
 	
 	});
 
 	req.write(this.loginParams);
-	req.end();
+	req.end();*/
 }
 
 /**
  * To logout the API.
  */
 blueskyconn.prototype.logout = function(){
-	var req = http.request(this.logoutOption, function(res) {
-		var t,u;
+	var self = this;
+
+	async.series([
+	function(callback){
+
+		var req = http.request(self.logoutOption, function(res) {
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+				self.emit(_e.logout_chunk, chunk);
+				
+			});
+		});
+
+		req.write(self.logoutParams);
+		req.end();
+
+		callback();
+	},function(callback){
+
+		var req = http.request(self.logoutOption, function(res) {
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+				self.emit(_e.logout_chunk, chunk);
+				self.emit(_e.logout_result, chunk);
+			});
+		});
+
+		req.write(self.logoutParams);
+		req.end();
+
+		callback();
+	}], function(err, results){
+		if(err){
+			throw err;
+		}
+		//console.log('series all done. ' + results);
+	});
+
+	/*var req = http.request(this.logoutOption, function(res) {
 		res.setEncoding('utf8');
-		u = res.on('data', t = function (chunk) {
+		res.on('data', function (chunk) {
 			//console.log(chunk);
-			return;
+			self.emit(_e.logout_chunk, chunk);
 		});
 	});
 
 	req.write(this.logoutParams);
-	req.end();
+	req.end();*/
 }
 
 module.exports = blueskyconn;
--- testlib.js
+++ testlib.js
@@ -2,7 +2,18 @@ var blueskyconn = require('./bluesky-cli.js');
 
 var conn = new blueskyconn('http://127.0.0.1:8189', 'guest', 'guest');
 conn.test();
-conn.list_ed();
+var listEd = conn.list_ed();
+console.log(listEd._events.body);
+listEd.on('list_ed', function(listingED){
+	console.log(listingED + '\r\n');
+	var ed_arr = JSON.parse(listingED).ETLog.EDConnStatement;
+	console.log(ed_arr);
+	console.log();
+	
+	for(var i = 0; i < ed_arr.length; i++){
+		console.log(ed_arr[i].EDCNAME + " (" + ed_arr[i].EDIP + ") ");
+	}
+});
 
 
 
